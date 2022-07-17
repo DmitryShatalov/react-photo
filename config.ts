17cae3280da1af25e9b51f48c19ef547cabc6e2f
Dmitry <shatal1@yandex,ru>
2022-07-17 13:10:08 +0700

    add eslint, prettier and test react buld

--- config.ts
+++ config.ts
@@ -10,5 +10,5 @@ export const config = {
         url: process.env.SUPABASE_URL,
         anonKey: process.env.SUPABASE_ANON_KEY,
         secretKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
-    }
-}
+    },
+};
--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
 import express, { Request, Response } from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { config } from './config';
 import { supabase } from './supabase';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = config.server.port ?? 5000;
+const publicPath = path.join(__dirname, '..', 'client/dist');
+console.log(publicPath)
+app.use(express.static(publicPath));
 
 app.get('/', async (req: Request, res: Response) => {
-  const { data: user, error } = await supabase.auth.api.createUser({
-    email: 'shatal1@yandex.ru',
-    password: '12345',
-    user_metadata: { name: 'Yoda' },
-    email_confirm: true,
-  });
-  console.log(user);
-  console.log(error);
-  res.send('<h2>Привет Express!</h2>');
+    console.log(publicPath)
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
